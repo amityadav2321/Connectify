bf76c8151b3224ef0f8264c127109dbc059e91d7
amityadav2321 <itsamityadav2307@gmail.com>
2025-07-07 03:30:00 +0530

    reverse

--- src/app.js
+++ src/app.js
@@ -22,10 +22,10 @@ app.use(cookieparser());
 // app.use("/", profileRouter);
 
 // ✅ Correct
-app.use("/api", authRouter);
-app.use("/api", profileRouter);
-app.use("/api", requestRouter);
-app.use("/api", userRouter);
+app.use("/", authRouter);
+app.use("/", profileRouter);
+app.use("/", requestRouter);
+app.use("/", userRouter);
 
 
 
