2d605601a9773bbb1b75659dd0259403bd022117
amityadav2321 <itsamityadav2307@gmail.com>
2025-07-08 23:25:08 +0530

    login route for render

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -74,9 +74,13 @@ authRouter.post("/login",async(req,res)=>{
             const token= await user.getJWT();
             
 
-            res.cookie("token",token,{
-                expires:new Date(Date.now()+8*3600000),
-            });
+                res.cookie("token", token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: "None",
+                expires: new Date(Date.now() + 8 * 3600000), // 8 hours
+                });
+
             res.send(user);
         }else{
             throw new Error("Invaild Credentials");
