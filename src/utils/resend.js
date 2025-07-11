1b1d910acf34abee46077c0f72c19e15e2ec895f
amityadav2321 <itsamityadav2307@gmail.com>
2025-07-11 23:41:30 +0530

    bug fix

--- src/utils/resend.js
+++ src/utils/resend.js
@@ -1,20 +1,21 @@
 // utils/resend.js
 const { Resend } = require('resend');
-
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendMail = async ({ to, subject, html }) => {
   try {
     const data = await resend.emails.send({
-      frofrom: 'onboarding@resend.dev',
-      reply_to: 'itsamityadav2307@gmail.com',
+      from: 'onboarding@resend.dev',     
       to,
       subject,
       html,
+      reply_to: 'itsamityadav2307@gmail.com',   
     });
-    console.log('Email sent:', data);
+    console.log('✅ Email sent:', data);
+    return data;
   } catch (error) {
-    console.error('Email send error:', error);
+    console.error('❌ Email send error:', error);
+    throw error;
   }
 };
 
